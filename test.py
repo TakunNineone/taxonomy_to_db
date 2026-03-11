7eb7c2810eb5068624a3658c4c43188f84167fff
Alexey Kosmenko <kkosmas@yandex.ru>
2026-03-11 18:11:45 +0300

    11032026

--- test.py
+++ test.py
@@ -1,10 +1,10 @@
 import pandas as pd
 from sqlalchemy import create_engine,text
-version = 'final52_cbr'
+version = 'final_8_0_11032026_1721'
 conn_string = f'postgresql+psycopg2://postgres:124kosm21@127.0.0.1/{version}'
 db = create_engine(conn_string,isolation_level="AUTOCOMMIT")
 conn = db.connect()
 
 
-roles_table_definition_6=pd.read_csv("def_table_5_2.csv",header=0,delimiter=';')
+roles_table_definition_6=pd.read_csv("def_table_8.csv",header=0,delimiter=';')
 roles_table_definition_6.to_sql('roles_table_definition', conn, if_exists='replace', index=False) #replace - очищает таблицу, append - добавляет строки
